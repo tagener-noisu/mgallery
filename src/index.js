28107e8366f65b8dab7c7a33b6d594c725ab4d1b
tagener-noisu <op4.renegat@gmail.com>
2021-01-06 16:00:04 +0600

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+const React = require('react');
+const ReactDOM = require('react-dom');
 
-ReactDOM.render(<h1>Hello React!</h1>, document.getElementById('root'));
+const root = document.getElementById('root');
+ReactDOM.render(<h1>Hello, Mastodon Gallery!</h1>, root);
