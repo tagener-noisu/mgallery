ad3073523edd6baa7dd05a1a0fdd921de8b1a417
tagener-noisu <op4.renegat@gmail.com>
2021-01-07 17:43:40 +0600

    Change constants for scrollin without delay

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const constants = {
-    number_of_items_to_load: 20,
-    preload_margin: 1000,
+    number_of_items_to_load: 50,
+    preload_margin: 2000,
     number_of_columns: 3,
     column_width: 400,
 }
