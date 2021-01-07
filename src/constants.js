24ced4820247b877b83bc3422440406a42d3cd1f
tagener-noisu <op4.renegat@gmail.com>
2021-01-07 17:40:41 +0600

    Move constants to a separate file

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+const constants = {
+    number_of_items_to_load: 20,
+    preload_margin: 1000,
+    number_of_columns: 3,
+    column_width: 400,
+}
+
+module.exports = constants;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
-
-const constants = {
-    number_of_items_to_load: 20,
-    preload_margin: 1000,
-    number_of_columns: 3,
-    column_width: 400,
-}
+const constants = require('./constants');
 
 function GalleryItem(props) {
     const {preview_url} = props.contents;
